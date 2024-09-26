53d52ccfd881832273573421906f800f02ec6487
PAZZI14 <pazinatto.tiago@gmail.com>
2024-09-26 11:43:52 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Adicione interatividade conforme necessário
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
